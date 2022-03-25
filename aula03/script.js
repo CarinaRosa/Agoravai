320153aeb377f37ce19e270c8732e82c9414b006
Carlos Henrique <carina_rosa_12@hotmail.com>
2022-03-24 21:20:13 -0300

    commit

--- aula03/script.js
+++ aula03/script.js
@@ -8,3 +8,6 @@ const botoes = document.querySelectorAll('button');
 for(var i = 0; i < botoes.length; i++) {
     botoes[i].addEventListener('click', criarParagrafo);
 }
+
+
+
--- aula04/script.js
+++ aula04/script.js
@@ -1,10 +1,17 @@
-function criarParagrafo() {
-    let para = document.createElement('p');
-    para.textContent = 'Você clicou no botão!';
-    document.body.appendChild(para);
+function contador() {
+    var list = []
+    for (let i = 0; i <= 10; i++) {
+        list.push(i)
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        document.getElementById("contador").innerHTML=list        
+    }
 }
 
-const botoes = document.querySelectorAll('button');
-for(var i = 0; i < botoes.length; i++) {
-    botoes[i].addEventListener('click', criarParagrafo);
+function debito() {
+    let paragrafo = document.createElement('p');
+    paragrafo.textContent = 'Também conhecido como dívida técnica, o débito técnico é um conceito no desenvolvimento de software utilizado para representar o custo implícito de uma implementação ou solução pensada somente no agora, elaborada para suprir demandas atuais, em vez fazer uso de uma abordagem de melhor qualidade.';
+    document.body.appendChild(paragrafo);
 }
+
