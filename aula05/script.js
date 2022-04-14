72c8f2ec211782bee46fc98b75c4ded8e80f159f
CarinaRosa <carina_rosa_12@hotmail.com>
2022-04-14 19:51:46 -0300

    update

--- aula05/script.js
+++ aula05/script.js
@@ -1,12 +1,37 @@
 function myFunction() 
         {
             var x;
-            var nome = prompt("Digite seu nome: ");
+            var nome = window.prompt("Digite seu nome: ");
 
             if (nome != null) {
                 x = "Nome: " + nome;
                 document.getElementById("demo").innerHTML=x;
+                
             }
 
+
+        }
+
+function myFunction() 
+        {
+            var x;
+            var n1 = prompt("Digite sua primeira nota: ");
+
+            if (n1 != null) {
+                x = "Nota 1: " + n1;
+                document.getElementById("demo").innerHTML=x;
+            }
+
+        }
+
+function myFunction() 
+        {
+            var x;
+            var n2 = prompt("Digite sua segunda nota: ");
+
+            if (n2 != null) {
+                x = "Nota 2: " + n2;
+                document.getElementById("demo").innerHTML=x;
+            }
 
         }
