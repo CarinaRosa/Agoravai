0d91d19a4f1906c8a965d814c2447792969414f1
CarinaRosa <carina_rosa_12@hotmail.com>
2022-05-19 21:11:38 -0300

    up

--- aula11/script.js
+++ aula11/script.js
@@ -17,3 +17,17 @@ const objs = [
 
 console.log(objs);
 console.log(typeof objs);
+
+const jsonData = JSON.stringify(objs);
+console.log(jsonData);
+console.log(typeof jsonData);
+
+const objjson = JSON.parse(jsonData);
+console.log(objjson);
+console.log(typeof objjson);
+
+objjson.map((aluno) => {
+    console.log(aluno.nome);
+    console.log(aluno.email);
+    console.log(aluno.idade);
+})
